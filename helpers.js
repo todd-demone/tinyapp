532429e9bee1e359eca7a8b00b31f726859e0f94
Todd Demone <tdemone1@gmail.com>
2021-11-29 15:18:46 +0000

    lint file

--- helpers.js
+++ helpers.js
@@ -3,7 +3,7 @@
  * @param {number} length The number of characters to be included in the returned string
  * @returns String a string of 6 random alphanumeric characters [A-Za-z0-9]
  */
-const generateRandomString = function (length) {
+const generateRandomString = function(length) {
   const CHARACTERS =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   const charactersLength = CHARACTERS.length;
@@ -15,22 +15,22 @@ const generateRandomString = function (length) {
   return result;
 };
 
-const getUserByEmail = function (email, database) {
-  for (user_id in database) {
-    if (database[user_id]["email"] === email) {
-      return database[user_id];
+const getUserByEmail = function(email, database) {
+  for (const userID in database) {
+    if (database[userID].email === email) {
+      return database[userID];
     }
   }
 };
 
-// Returns the URLs where the user_id is equal to the user_id of the logged in user.
-const urlsForUser = function (user_id, urlDatabase) {
+// Returns the URLs where the userID is equal to the userID of the logged in user.
+const urlsForUser = function(userID, urlDatabase) {
   const results = {};
   const allShortURLs = Object.keys(urlDatabase);
   const filteredShortURLs = allShortURLs.filter(
-    (shortURL) => urlDatabase[shortURL].userID === user_id
+    (shortURL) => urlDatabase[shortURL].userID === userID
   );
-  for (shortURL of filteredShortURLs) {
+  for (const shortURL of filteredShortURLs) {
     results[shortURL] = urlDatabase[shortURL];
   }
   return results;

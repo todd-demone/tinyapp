24a6c1542c5b1b94713d84b69c3c5ed41dfaec28
Todd Demone <tdemone1@gmail.com>
2021-11-29 15:37:16 +0000

    add docstrings to helper functions

--- helpers.js
+++ helpers.js
@@ -1,7 +1,7 @@
 /**
- * Returns a string of 6 random alphanumeric characters
- * @param {number} length The number of characters to be included in the returned string
- * @returns String a string of 6 random alphanumeric characters [A-Za-z0-9]
+ * Returns a string of 6 random alphanumeric characters.
+ * @param {number} length The number of characters to be included in the returned string.
+ * @returns {string} 6 random alphanumeric characters [A-Za-z0-9].
  */
 const generateRandomString = function(length) {
   const CHARACTERS =
@@ -15,6 +15,12 @@ const generateRandomString = function(length) {
   return result;
 };
 
+/**
+ * Lookup a user using their email address; returns the object representing that user.
+ * @param {string} email the user's email address.
+ * @param {object} database a 'users' object; key is the userID; value is an object with id, email and password.
+ * @returns {object} a single user object.
+ */
 const getUserByEmail = function(email, database) {
   for (const userID in database) {
     if (database[userID].email === email) {
@@ -23,7 +29,12 @@ const getUserByEmail = function(email, database) {
   }
 };
 
-// Returns the URLs where the userID is equal to the userID of the logged in user.
+/**
+ * Returns the URLs where the userID is equal to the userID of the logged in user.
+ * @param {string} userID
+ * @param {object} urlDatabase a 'URLS' object; key is the shortURL; value is an object with longURL and userID.
+ * @returns {object} a single URL object.
+ */
 const urlsForUser = function(userID, urlDatabase) {
   const results = {};
   const allShortURLs = Object.keys(urlDatabase);

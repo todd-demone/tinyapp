81c3d7d808af0a65463ba829a3daf217a2ac6b06
Todd Demone <tdemone1@gmail.com>
2021-12-02 22:09:44 +0000

    refactor routes - took curly braces out of one-liner if statements; assigned req.params and req.body to variables

--- routers/urlRouter.js
+++ routers/urlRouter.js
@@ -7,10 +7,8 @@ const { urlsForUser, generateRandomString } = require("../helpers");
 const urlRouter = (users, urlDatabase) => {
 
   router.get("/", (req, res) => {
-    // User is not logged in
-    if (!req.session.userID) {
-      return res.redirect("/users/login");
-    }
+    if (!req.session.userID) return res.redirect("/users/login");
+    
     const templateVars = {
       urls: urlsForUser(req.session.userID, urlDatabase),
       user: users[req.session.userID],
@@ -19,20 +17,17 @@ const urlRouter = (users, urlDatabase) => {
   });
 
   router.get("/new", (req, res) => {
-    // User is not logged in
-    if (!req.session.userID) {
-      return res.redirect("/users/login");
-    }
+    if (!req.session.userID) return res.redirect("/users/login");
     const templateVars = { user: users[req.session.userID] };
     res.render("urls_new", templateVars);
   });
 
   router.post("/", (req, res) => {
-    // User is not logged in
-    if (!req.session.userID) {
-      return res.sendStatus(401);
-    // User submitted an empty form
-    } else if (!req.body.longURL) {
+    const { longURL } = req.body;
+    const shortURL = generateRandomString();
+    
+    if (!req.session.userID) return res.sendStatus(401);
+    if (!longURL) {
       const templateVars = {
         user: users[req.session.userID],
         code: 400,
@@ -41,9 +36,8 @@ const urlRouter = (users, urlDatabase) => {
       return res.status(400).render("error", templateVars);
     }
 
-    const shortURL = generateRandomString();
     urlDatabase[shortURL] = {
-      longURL: req.body.longURL,
+      longURL: longURL,
       userID: req.session.userID,
       visitorIDs: [],
       visitLog: [],
@@ -52,19 +46,17 @@ const urlRouter = (users, urlDatabase) => {
   });
 
   router.get("/:shortURL", (req, res) => {
-    // User is not logged in
-    if (!req.session.userID) {
-      return res.redirect("/users/login");
-    // The shortURL doesn't exist
-    } else if (!(req.params.shortURL in urlDatabase)) {
+    const { shortURL } = req.params;
+    if (!req.session.userID) return res.redirect("/users/login");
+    if (!(shortURL in urlDatabase)) {
       const templateVars = {
         user: users[req.session.userID],
         code: 404,
         message: "Page not found",
       };
       return res.status(404).render("error", templateVars);
-    // The URL belongs to another user (no permission)
-    } else if (req.session.userID !== urlDatabase[req.params.shortURL].userID) {
+    }
+    if (req.session.userID !== urlDatabase[shortURL].userID) {
       const templateVars = {
         user: users[req.session.userID],
         code: 403,
@@ -72,53 +64,41 @@ const urlRouter = (users, urlDatabase) => {
       };
       return res.status(403).render("error", templateVars);
     }
+    
     const templateVars = {
-      shortURL: req.params.shortURL,
-      urlData: urlDatabase[req.params.shortURL],
+      shortURL,
+      urlData: urlDatabase[shortURL],
       user: users[req.session.userID],
     };
     res.render("urls_show", templateVars);
   });
 
-  // PUT /urls/:shortURL -> updates a specific URL
   router.put("/:shortURL", (req, res) => {
-    // User is not logged in
-    if (!req.session.userID) {
-      return res.sendStatus(401);
-    // The shortURL doesn't exist
-    } else if (!(req.params.shortURL in urlDatabase)) {
-      return res.sendStatus(404);
-    // User submitted an empty form
-    } else if (!req.body.longURL) {
+    const { shortURL } = req.params;
+    const { longURL } = req.body;
+    if (!req.session.userID) return res.sendStatus(401);
+    if (!(shortURL in urlDatabase)) return res.sendStatus(404);
+    if (req.session.userID !== urlDatabase[shortURL].userID) return res.sendStatus(403);
+    if (!longURL) {
       const templateVars = {
         user: users[req.session.userID],
         code: 400,
         message: "You didn't edit the URL. Please try again.",
       };
       return res.status(400).render("error", templateVars);
-    // The URL belongs to another user (no permission)
-    } else if (req.session.userID !== urlDatabase[req.params.shortURL].userID) {
-      return res.sendStatus(403);
     }
 
-    urlDatabase[req.params.shortURL].longURL = req.body.longURL;
+    urlDatabase[shortURL].longURL = longURL;
     res.redirect("/urls");
   });
 
-  // DELETE /urls/:shortURL/delete => deletes a specific URL
   router.delete("/:shortURL", (req, res) => {
-    // User is  not logged in
-    if (!req.session.userID) {
-      return res.sendStatus(401);
-    // The shortURL doesn't exist
-    } else if (!(req.params.shortURL in urlDatabase)) {
-      return res.sendStatus(404);
-    // The URL belongs to another user (no permission)
-    } else if (req.session.userID !== urlDatabase[req.params.shortURL].userID) {
-      return res.sendStatus(403);
-    }
+    const { shortURL } = req.params;
+    if (!req.session.userID) return res.sendStatus(401);
+    if (!(shortURL in urlDatabase)) return res.sendStatus(404);
+    if (req.session.userID !== urlDatabase[shortURL].userID) return res.sendStatus(403);
 
-    delete urlDatabase[req.params.shortURL];
+    delete urlDatabase[shortURL];
     res.redirect("/urls");
   });
 
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -13,7 +13,10 @@ const userRouter = (users) => {
   });
 
   router.post("/register", (req, res) => {
-    if (!req.body.email || !req.body.password) {
+    const { email, password } = req.body;
+    const user = getUserByEmail(email, users);
+    
+    if (!email || !password) {
       const templateVars = {
         user: users[req.session.userID],
         code: 400,
@@ -21,22 +24,24 @@ const userRouter = (users) => {
       };
       return res.status(400).render("error", templateVars);
     }
-    const existingUser = getUserByEmail(req.body.email, users);
-    if (existingUser) {
+    if (user) {
       const templateVars = {
-        user: existingUser.id,
+        user: user.id,
         code: 400,
         message: "This account already exists. Please login.",
       };
       return res.status(400).render("error", templateVars);
     }
+    
     const userID = generateRandomString();
+    const hashedPassword = bcrypt.hashSync(password, 10);
     users[userID] = {
       id: userID,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, 10),
+      email,
+      password: hashedPassword,
       visitorIDs: {},
     };
+    
     req.session.userID = userID;
     req.session.visitorIDs = users[userID].visitorIDs;
     res.redirect("/urls");
@@ -48,24 +53,20 @@ const userRouter = (users) => {
   });
 
   router.post("/login", (req, res) => {
-    const existingUser = getUserByEmail(req.body.email, users);
-    if (
-      existingUser &&
-      req.body.password &&
-      bcrypt.compareSync(req.body.password, existingUser.password)
-    ) {
-      req.session.userID = existingUser.id;
-      req.session.visitorIDs = existingUser.visitorIDs;
-      res.redirect("/urls");
-    } else {
-      const templateVars = {
-        user: users[req.session.userID],
-        code: 403,
-        message:
-          "Email or password are incorrect. Please try again, or register if you don't have an account.",
-      };
-      res.status(403).render("error", templateVars);
+    const { email, password } = req.body;
+    const user = getUserByEmail(email, users);
+    if (user && password && bcrypt.compareSync(password, user.password)) {
+      req.session.userID = user.id;
+      req.session.visitorIDs = user.visitorIDs;
+      return res.redirect("/urls");
     }
+    const templateVars = {
+      user: users[req.session.userID],
+      code: 403,
+      message:
+        "Email or password are incorrect. Please try again, or register if you don't have an account.",
+    };
+    res.status(403).render("error", templateVars);
   });
   
   router.post("/logout", (req, res) => {

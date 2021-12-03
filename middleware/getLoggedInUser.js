a414ab2396db0f395c81385ccb7dd1723fda83b5
Todd Demone <tdemone1@gmail.com>
2021-12-03 19:34:20 +0000

    eslint fix

--- middleware/getLoggedInUser.js
+++ middleware/getLoggedInUser.js
@@ -1,5 +1,5 @@
-const getLoggedInUser = function (templateVars, users) {
-  return function (req, res, next) {
+const getLoggedInUser = function(templateVars, users) {
+  return function(req, res, next) {
     const { userID } = req.session;
     const user = users[userID];
     templateVars.user = user ? { id: userID, email: user.email } : null;

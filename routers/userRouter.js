fdbd2e6092556bac93284cd75c3f0e67c1345821
Todd Demone <tdemone1@gmail.com>
2021-12-03 14:54:31 +0000

    refactor error messages

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -15,19 +15,18 @@ const userRouter = (templateVars, users) => {
   router.post("/register", (req, res) => {
     const { email, password } = req.body;
     const user = getUserByEmail(email, users);
-    if (!email || !password) {
-      templateVars.code = 400;
-      templateVars.message = "Email or password are empty. Please try again.";
-      return res.status(400).render("error", templateVars);
-    }
-    if (user) {
-      templateVars.code = 400;
-      templateVars.message = "This account already exists. Please login.";
-      return res.status(400).render("error", templateVars);
+    
+    const errors = [];
+    if (user) errors.push({ msg: "Error - this account already exists." });
+    if (!email || !password) errors.push({ msg: "Error - email or password are empty. Please try again." });
+    if (errors.length) {
+      templateVars.errors = errors;
+      return res.status(400).render("register", templateVars);
     }
     const userID = generateRandomString();
     const visitorID = generateRandomString();
     const hashedPassword = bcrypt.hashSync(password, 10);
+    
     users[userID] = {
       id: userID,
       email,
@@ -41,20 +40,27 @@ const userRouter = (templateVars, users) => {
 
   router.get("/login", (req, res) => {
     if (templateVars.user) return res.redirect("/urls");
-    res.render("login", { user: null });
+    res.render("login", templateVars);
   });
 
   router.post("/login", (req, res) => {
     const { email, password } = req.body;
     const user = getUserByEmail(email, users);
+    
     if (user && password && bcrypt.compareSync(password, user.password)) {
       req.session.userID = user.id;
       req.session.visitorID = user.visitorID;
       return res.redirect("/urls");
+    } 
+    
+    const errors = [];
+    if (!user) errors.push({ msg: "Error - email address can't be found."} );
+    else if (!(bcrypt.compareSync(password, user.password))) errors.push({ msg: "Error - password is incorrect."} );
+    if (errors.length) {
+      templateVars.errors = errors;
+      return res.status(403).render("login", templateVars);
     }
-    templateVars.code = 403;
-    templateVars.message = "Email or password are incorrect. Please try again, or register if you don't have an account.";
-    res.status(403).render("error", templateVars);
+     
   });
   
   router.post("/logout", (req, res) => {

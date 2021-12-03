e8bfe0dae896d40939f302e7d84c199776301da7
Todd Demone <tdemone1@gmail.com>
2021-12-03 10:37:43 +0000

    refactor getLoggedInUser() sit resembles traditional function declaration (for my own benefit)

--- middleware/getLoggedInUser.js
+++ middleware/getLoggedInUser.js
@@ -1,8 +1,10 @@
-const getLoggedInUser = (templateVars, users) => (req, res, next) => {
-  const { userID } = req.session;
-  const user = users[userID];
-  templateVars.user = user ? { id: userID, email: user.email } : null;
-  next();
+const getLoggedInUser = function(templateVars, users) {
+  return function(req, res, next) {
+    const { userID } = req.session;
+    const user = users[userID];
+    templateVars.user = user ? { id: userID, email: user.email } : null;
+    next();
+  }
 };
 
 module.exports = getLoggedInUser;

1f35ed04b573080cc86ea4146d1522ec0d1ae7e8
Todd Demone <tdemone1@gmail.com>
2021-12-03 14:55:33 +0000

    add errors property to templateVars

--- middleware/getLoggedInUser.js
+++ middleware/getLoggedInUser.js
@@ -3,6 +3,7 @@ const getLoggedInUser = function(templateVars, users) {
     const { userID } = req.session;
     const user = users[userID];
     templateVars.user = user ? { id: userID, email: user.email } : null;
+    templateVars.errors = null;
     next();
   }
 };

798d6c1927e28e534af3ccf617e9e99238c28730
Todd Demone <tdemone1@gmail.com>
2021-12-03 01:24:28 +0000

    add getLoggedInUser middleware; change calls to routers to include templateVars

--- express_server.js
+++ express_server.js
@@ -3,6 +3,8 @@ const bodyParser = require("body-parser");
 const cookieSession = require("cookie-session");
 const methodOverride = require('method-override');
 
+const getLoggedInUser = require('./middleware/getLoggedInUser');
+
 const userRouter = require('./routers/userRouter');
 const urlRouter = require('./routers/urlRouter');
 const uRouter = require('./routers/uRouter');
@@ -12,6 +14,7 @@ const port = 8080;
 
 const urlDatabase = {}; // b6UTxQ: { longURL: "http://www.tsn.ca", userID: "aJ48lW", visitorIDs: [abc123, def456, ghi789], visitLog: [{ timestamp: 2021-12..., visitorID: abc123 }] },
 const users = {}; // userRandomID: { id: "userRandomID", email: "user@example.com", password: "hashed-password", visitorIDs: { shortURL1: myVisitorID1, shortURL2: myVisitorID2 } },
+const templateVars = {};
 
 app.set("view engine", "ejs");
 
@@ -19,8 +22,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ name: "session", keys: ["key1", "key2"] }));
 app.use(methodOverride('_method'));
 
-app.use("/users", userRouter(users));
-app.use("/urls", urlRouter(users, urlDatabase));
+app.use(getLoggedInUser(templateVars, users));
+
+app.use("/users", userRouter(templateVars, users));
+app.use("/urls", urlRouter(templateVars, users, urlDatabase));
 app.use("/u", uRouter(users, urlDatabase));
 
 // catch-all for non-existing pages
